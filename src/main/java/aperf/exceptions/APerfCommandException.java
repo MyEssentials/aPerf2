4643fb4827f64f91d354b8bd17e8a419b3ebb2ce
Joe Goett <jgoett154@gmail.com>
2015-07-20 23:14:38 -0400

    Exception localization! :D

--- src/main/java/aperf/exceptions/APerfCommandException.java
+++ src/main/java/aperf/exceptions/APerfCommandException.java
@@ -1,10 +1,11 @@
 package aperf.exceptions;
 
+import aperf.proxy.LocalizationProxy;
 import net.minecraft.command.CommandException;
 
 public class APerfCommandException extends CommandException {
     public APerfCommandException(String key, Object... args) {
-        super(key, args);
+        super(LocalizationProxy.getLocalization().getLocalization(key, args));
     }
 
     public APerfCommandException(String key, Throwable cause, Object... args) {
--- src/main/java/aperf/exceptions/APerfWrongUsageException.java
+++ src/main/java/aperf/exceptions/APerfWrongUsageException.java
@@ -1,9 +1,10 @@
 package aperf.exceptions;
 
+import aperf.proxy.LocalizationProxy;
 import net.minecraft.command.WrongUsageException;
 
 public class APerfWrongUsageException extends WrongUsageException {
     public APerfWrongUsageException(String key, Object... args) {
-        super(key, args);
+        super(LocalizationProxy.getLocalization().getLocalization(key, args));
     }
 }
